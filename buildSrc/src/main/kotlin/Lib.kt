5b843ab2f232fcc808ec869927feb6c7d7657c80
Björn Petersen <pheasn@gmail.com>
2019-02-27 21:34:55 +0100

    Update YouTube dependency to 0.13.0
    Fixes #4

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -3,7 +3,7 @@ object Lib {
     const val KOTLIN_LOGGING = "1.6.25"
 
     const val MUSICBOT = "0.18.1"
-    const val YOUTUBE_PROVIDER = "0.12.0"
+    const val YOUTUBE_PROVIDER = "0.13.0"
 
     const val JUNIT = "5.4.0"
 }
