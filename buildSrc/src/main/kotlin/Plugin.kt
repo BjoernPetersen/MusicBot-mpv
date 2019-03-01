34519b511039ea405d81910914268f0192cf6985
Björn Petersen <pheasn@gmail.com>
2019-03-01 10:39:37 +0100

    Use ShadowJar plugin 5.0.0

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -4,5 +4,5 @@ object Plugin {
     const val KOTLIN = "1.3.21"
     const val SPOTBUGS_PLUGIN = "1.6.10"
     const val SPOTBUGS_TOOL = "3.1.11"
-    const val SHADOW_JAR = "4.0.4"
+    const val SHADOW_JAR = "5.0.0"
 }
