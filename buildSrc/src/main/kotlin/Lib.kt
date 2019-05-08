a4a02deb3c05dd2f527405ea870d6014ec787541
Björn Petersen <pheasn@gmail.com>
2019-05-08 12:43:34 +0200

    Use release version of MusicBot

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -1,7 +1,7 @@
 object Lib {
     const val KOTLIN = Plugin.KOTLIN
 
-    const val MUSICBOT = "0.21.0-SNAPSHOT"
+    const val MUSICBOT = "0.21.0"
     const val YOUTUBE_PROVIDER = "0.15.0"
     const val NU_PROCESS = "1.2.4"
 
