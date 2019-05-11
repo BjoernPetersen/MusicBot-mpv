771e0e370bea5f613a706ed02fd0b7817b99a1ed
Björn Petersen <pheasn@gmail.com>
2019-05-11 15:59:03 +0200

    Use release core

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -1,7 +1,7 @@
 object Lib {
     const val KOTLIN = Plugin.KOTLIN
 
-    const val MUSICBOT = "0.22.0-SNAPSHOT"
+    const val MUSICBOT = "0.22.0"
     const val YOUTUBE_PROVIDER = "0.16.0"
     const val NU_PROCESS = "1.2.4"
 
